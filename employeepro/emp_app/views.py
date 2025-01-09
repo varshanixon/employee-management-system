6ac9cdfea131b03de0dc07af5f56e9f321653ba3
varshanixon <varshanixon17@gmail.com>
2025-01-10 00:20:44 +0530

    Update views.py
    Created EmployeeCreateView with get method

--- employeepro/emp_app/views.py
+++ employeepro/emp_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
+from django.views.generic import View
+
+from emp_app.forms import EmployeeForm
+
 # Create your views here.
+
+class EmployeeCreateView(View):
+
+    template_name = "emp_add.html"
+
+    form_class = EmployeeForm
+
+    def get(self,request,*args,**kwargs):
+
+        # Create form instance
+        form_instance = self.form_class
+
+        # Render form into template
+        return render(request,self.template_name,{'form':form_instance})
